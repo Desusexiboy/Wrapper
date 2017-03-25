dbbfdb8540684906901f7f18f7cc9eb44b4ea492
AlexShpil <alexshpil@gmail.com>
2017-03-25 12:07:47 +0300

    Stage: 1. Argparse. Receives known parameters from CLI input.

--- wrapper.py
+++ wrapper.py
@@ -1,16 +1,43 @@
-#!/usr/bin/python
-import os
+'''
+PyAuthomation training task: Rsync wrapper.Stage 1: Argparse.
+Takes known parameters from CLI input. Returns output_dict.
+All keys stored in output_dict['keys'].
+All recognised files,dirrectories to copy and remote dirrectory is
+stored in output_dict['host_files']
+Password to remote host located in output_dict['password']
+'''
+
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("target", type=str,
-                    help="path to dir or file")
-parser.add_argument("adress_to_copy", type=str,
-                    help="remote adress which will store copy")
-parser.add_argument("-v", "--verbose", action="store_true",
-                    help="increase output verbosity")
-args = parser.parse_args()
-if args.verbose:
-    print("Copy local {} to remote machine with adress {}".format(args.target, args.adress_to_copy))
-else:
-    print(args.target, args.adress)
-os.system("rsync {0} {1}".format(args.target, args.adress_to_copy))
+
+
+def inputparse():
+    output_dict = {}
+    single_param = tuple('PavSzqi')
+    parser = argparse.ArgumentParser()
+    filesgarbage = []
+    unknownkeys = []
+    keys = []
+    parser.add_argument('-process', action="store_true", default=False)
+    parser.add_argument('-pass', action="store", dest="userpass", type=str)
+    parser.add_argument('-e', action="store", dest='connection', type=str)
+    parser.add_argument('files', type=str, help='list of files and dirrs to copy', nargs='*')
+    known, unknown = parser.parse_known_args()
+    # Fill arguments in group
+    for i in unknown:
+        if i[1:] in single_param:
+                keys.append(i)
+        else:
+            unknownkeys.append(i)
+    for i in known.files:
+        filesgarbage.append(i)
+    if known.connection == 'ssh':
+        keys.append('-e ssh')
+    elif known.connection == 'rsh':
+        keys.append('-e rsh')
+    if known.process:
+        keys.append('-process')
+    output_dict.update({'host_files': filesgarbage, 'keys': keys, 'password': known.userpass})
+    unknownkeys = set(unknownkeys)
+    return output_dict, list(unknownkeys)
+
+print inputparse()
